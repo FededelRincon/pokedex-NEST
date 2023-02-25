6af6b1c7f17ad06a17b12c28cd6c672223a070c2
FededelRincon <delrincon@gmail.com>
2023-02-25 12:55:29 -0300

    commonModule created + customPipe ParseMongoId created + deleteOne with validation done

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -92,8 +92,20 @@ export class PokemonService {
 //     return PokemonActualizado;
 // }
 
-  remove(id: number) {
-    return `This action removes a #${id} pokemon`;
+  async remove( id: string ) {
+    // const pokemon = await this.findOne( id )
+    // await pokemon.deleteOne(); 
+    // return { id };
+
+    // const result = await this.pokemonModel.findByIdAndDelete( id )
+
+    const { deletedCount } = await this.pokemonModel.deleteOne({ _id: id })
+
+    if( deletedCount === 0){
+      throw new BadRequestException(`Pokemon with id '${id}' not found`)
+    }
+
+    return;
   }
 
 
