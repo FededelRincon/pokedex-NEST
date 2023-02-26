db7ea5e4edcf6678c48740b99dece53aceb48d56
FededelRincon <delrincon@gmail.com>
2023-02-26 19:02:43 -0300

    update readme + delete DB and insertMany in clean DB

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -18,18 +18,29 @@ export class SeedService {
   ){}
 
   async executeSeed() {
-    const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=10')
 
-    data.results.forEach( async ({name, url}) => {
+    //Borro la tabla
+    await this.pokemonModel.deleteMany({}); 
+
+    // Y a partir de aca la comienzo a llenar
+    const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=500')
+
+    const pokemonToInsert: {name: string, no:number}[] = [];
+
+    data.results.forEach(({name, url}) => {
 
       const segments = url.split('/')
       const no = +segments[ segments.length - 2 ]
 
-      const pokemon = await this.pokemonModel.create( {name, no} )
-      
-    })
+      // const pokemon = await this.pokemonModel.create( {name, no} )
+      pokemonToInsert.push({ name, no })        
+
+    });
+
+    await this.pokemonModel.insertMany(pokemonToInsert)
 
     return {'seed executed': true}
+
   }
 
 }
